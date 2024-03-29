bd4cd7bace1feb1d680d5c76d021d90056e33218
Reza Masoudi <dev.masoudi@gmail.com>
2024-03-29 08:25:47 +0330

    doc: add facade driver method doc lock

--- src/Facade/SMS.php
+++ src/Facade/SMS.php
@@ -3,7 +3,11 @@
 namespace Masoudi\SMS\Facade;
 
 use Illuminate\Support\Facades\Facade;
+use Masoudi\SMS\Contracts\BaseSmsDriver;
 
+/**
+ * @method static BaseSmsDriver driver(?\Closure $callback = null, ?string $driver = null)
+ */
 class SMS extends Facade
 {
     protected static function getFacadeAccessor(): string
